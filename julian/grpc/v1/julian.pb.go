19a545459319fe34a3fd5b2f7be0bf0328e0da70
Shane Howearth <Shane.h.1@gmail.com>
2019-06-10 17:34:04 +1000

    Use the right name (Convert not Create)  for the function call

--- julian/grpc/v1/julian.pb.go
+++ julian/grpc/v1/julian.pb.go
@@ -135,7 +135,7 @@ func init() {
 func init() { proto.RegisterFile("julian.proto", fileDescriptor_838069e7f4e90ff2) }
 
 var fileDescriptor_838069e7f4e90ff2 = []byte{
-	// 195 bytes of a gzipped FileDescriptorProto
+	// 191 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0xc9, 0x2a, 0xcd, 0xc9,
 	0x4c, 0xcc, 0xd3, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x2a, 0x33, 0x54, 0x4a, 0xe0, 0xe2,
 	0x73, 0xce, 0xcf, 0x2b, 0x4b, 0x2d, 0x2a, 0x09, 0x4a, 0x2d, 0x2c, 0x4d, 0x2d, 0x2e, 0x11, 0x12,
@@ -144,11 +144,10 @@ var fileDescriptor_838069e7f4e90ff2 = []byte{
 	0x17, 0x73, 0x4a, 0x62, 0xa5, 0x04, 0x33, 0x58, 0x0c, 0xc4, 0x04, 0xe9, 0xcd, 0xc8, 0x2f, 0x2d,
 	0x92, 0x60, 0x51, 0x60, 0xd4, 0x60, 0x0c, 0x02, 0xb3, 0x95, 0x2c, 0xb9, 0xf8, 0xe1, 0x36, 0x14,
 	0x17, 0xe4, 0xe7, 0x15, 0xa7, 0x0a, 0xa9, 0x71, 0xf1, 0x41, 0x1c, 0xe2, 0x92, 0x58, 0x92, 0x1a,
-	0x92, 0x99, 0x9b, 0x0a, 0xb6, 0x8c, 0x31, 0x08, 0x4d, 0xd4, 0xc8, 0x89, 0x8b, 0xd7, 0x0b, 0x2c,
-	0x12, 0x9c, 0x5a, 0x54, 0x96, 0x99, 0x9c, 0x2a, 0x64, 0xc8, 0xc5, 0xe6, 0x5c, 0x94, 0x9a, 0x58,
-	0x92, 0x2a, 0x24, 0xa4, 0x57, 0x66, 0xa8, 0x87, 0xea, 0x72, 0x29, 0x61, 0x14, 0x31, 0x88, 0x5d,
-	0x49, 0x6c, 0x60, 0xbf, 0x1a, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0xf2, 0xaa, 0x7b, 0x06, 0xfb,
-	0x00, 0x00, 0x00,
+	0x92, 0x99, 0x9b, 0x0a, 0xb6, 0x8c, 0x31, 0x08, 0x4d, 0xd4, 0xc8, 0x99, 0x8b, 0xd7, 0x0b, 0x2c,
+	0x12, 0x9c, 0x5a, 0x54, 0x96, 0x99, 0x9c, 0x2a, 0x64, 0xc4, 0xc5, 0x0e, 0x35, 0x4b, 0x48, 0x48,
+	0xaf, 0xcc, 0x50, 0x0f, 0xd5, 0xe9, 0x52, 0xc2, 0x28, 0x62, 0x10, 0xcb, 0x92, 0xd8, 0xc0, 0x9e,
+	0x35, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x09, 0xbd, 0x80, 0xab, 0xfc, 0x00, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -163,8 +162,8 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type JulianServiceClient interface {
-	// Create new todo task
-	Create(ctx context.Context, in *ConvertRequest, opts ...grpc.CallOption) (*ConvertResponse, error)
+	// Convert to Julian date
+	Convert(ctx context.Context, in *ConvertRequest, opts ...grpc.CallOption) (*ConvertResponse, error)
 }
 
 type julianServiceClient struct {
@@ -175,9 +174,9 @@ func NewJulianServiceClient(cc *grpc.ClientConn) JulianServiceClient {
 	return &julianServiceClient{cc}
 }
 
-func (c *julianServiceClient) Create(ctx context.Context, in *ConvertRequest, opts ...grpc.CallOption) (*ConvertResponse, error) {
+func (c *julianServiceClient) Convert(ctx context.Context, in *ConvertRequest, opts ...grpc.CallOption) (*ConvertResponse, error) {
 	out := new(ConvertResponse)
-	err := c.cc.Invoke(ctx, "/v1.JulianService/Create", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/v1.JulianService/Convert", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -186,28 +185,28 @@ func (c *julianServiceClient) Create(ctx context.Context, in *ConvertRequest, op
 
 // JulianServiceServer is the server API for JulianService service.
 type JulianServiceServer interface {
-	// Create new todo task
-	Create(context.Context, *ConvertRequest) (*ConvertResponse, error)
+	// Convert to Julian date
+	Convert(context.Context, *ConvertRequest) (*ConvertResponse, error)
 }
 
 func RegisterJulianServiceServer(s *grpc.Server, srv JulianServiceServer) {
 	s.RegisterService(&_JulianService_serviceDesc, srv)
 }
 
-func _JulianService_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _JulianService_Convert_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ConvertRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(JulianServiceServer).Create(ctx, in)
+		return srv.(JulianServiceServer).Convert(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/v1.JulianService/Create",
+		FullMethod: "/v1.JulianService/Convert",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(JulianServiceServer).Create(ctx, req.(*ConvertRequest))
+		return srv.(JulianServiceServer).Convert(ctx, req.(*ConvertRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -217,8 +216,8 @@ var _JulianService_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*JulianServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "Create",
-			Handler:    _JulianService_Create_Handler,
+			MethodName: "Convert",
+			Handler:    _JulianService_Convert_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
