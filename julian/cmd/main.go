68dbaaaa9c164166ba71d79ecbe417b75ec5f59b
Shane Howearth <Shane.h.1@gmail.com>
2019-06-18 15:03:05 +1000

    Implement all of the methods defined in the protobuf file on the server

--- julian/cmd/main.go
+++ julian/cmd/main.go
@@ -21,7 +21,7 @@ const (
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatal("Failed to listen: %v", err)
+		log.Fatalf("Failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	v1.RegisterJulianServiceServer(s, &server{})
@@ -31,8 +31,28 @@ func main() {
 	}
 }
 
-// Create -
-func (s *server) Create(ctx context.Context, req *v1.ConvertRequest) (*v1.ConvertResponse, error) {
+// Convert -
+func (s *server) Convert(ctx context.Context, req *v1.ConvertRequest) (*v1.JulianResponse, error) {
 	jd := julian.GetJulianDay(req.GetYear(), req.GetMonth(), req.GetDay(), req.GetHour())
-	return &v1.ConvertResponse{JulianDateTime: jd}, nil
+	return &v1.JulianResponse{JulianDateTime: jd}, nil
+}
+
+// TimeJulianCentury -
+func (s *server) TimeJulianCentury(ctx context.Context, req *v1.JulianRequest) (*v1.JulianResponse, error) {
+	tjc := julian.TimeJulianCentury(req.GetJulianDateTime())
+	return &v1.JulianResponse{JulianDateTime: tjc}, nil
+}
+
+// JulianDayFromJulianCentury -
+func (s *server) JulianDayFromJulianCentury(ctx context.Context, req *v1.JulianRequest) (*v1.JulianResponse, error) {
+
+	tjc := julian.JulianDayFromJulianCentury(req.GetJulianDateTime())
+	return &v1.JulianResponse{JulianDateTime: tjc}, nil
+}
+
+// DayFromJulianDay -
+func (s *server) DayFromJulianDay(ctx context.Context, req *v1.JulianRequest) (*v1.CalendarResponse, error) {
+
+	y, m, d := julian.DayFromJulianDay(req.GetJulianDateTime())
+	return &v1.CalendarResponse{Year: y, Month: m, Day: d}, nil
 }
